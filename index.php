d234121d04d30e9b663af44093d662fdff75bf3f
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-24 17:22:22 +0100

    completed basic assignment

--- index.php
+++ index.php
@@ -25,16 +25,20 @@ $edited_paragraph = str_replace($bad_word, $substitution, $paragraph);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Torta di <?php echo $substitution ?></title>
 </head>
 
 <body>
 
-    <header>
-
+    <header class="container">
+        <h1 class="my-5">Torta di <?php echo $substitution ?></h1>
+        <form action=""></form>
     </header>
     <main>
-        <p> <strong><?php echo $edited_paragraph ?></strong> </p>
+        <div class="container">
+            <p><?php echo $edited_paragraph ?></p>
+        </div>
     </main>
 
 </body>
